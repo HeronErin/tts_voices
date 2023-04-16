0aa537f248786d04dabbe61e7f5c46a67935a3f0
pickle <you@example.com>
2023-04-16 17:01:02 -0400

    asd

--- makeSynthetic.py
+++ makeSynthetic.py
@@ -3,8 +3,6 @@ import torchaudio
 import torch.nn as nn
 import torch.nn.functional as F
 
-import IPython
-
 from tortoise.api import TextToSpeech
 from tortoise.utils.audio import load_audio, load_voice, load_voices
 
